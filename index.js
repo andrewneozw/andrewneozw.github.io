3ad2289751114c3e29d394c57d6c0085956df1a3
Andrew <neo.zhiwei.88@gmail.com>
2017-09-21 09:09:14 +0800

    javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("Welcome to JS class")
+
+var student = "zhiwei"
+
+var student_1 = "zhiwei"
+var student_2 = "bala"
+
+var welcome_msg = "Hi, We are friends.." + student_1 + "," + student_2
+console.log (welcome_msg)
