9b736537f2c931e494fb53918954c78b1c7e358e
Andrew <neo.zhiwei.88@gmail.com>
2017-09-21 12:45:06 +0800

    java

--- index.js
+++ index.js
@@ -6,4 +6,24 @@ var student_1 = "zhiwei"
 var student_2 = "bala"
 
 var welcome_msg = "Hi, We are friends.." + student_1 + "," + student_2
-console.log (welcome_msg)
+console.log(welcome_msg)
+
+function greet(nameOfPerson) {
+    var greeting_message = "Welcome, Mr" + nameOfPerson
+    console.log(greeting_message)
+}
+
+greet("Bala")
+
+greet("zhiwei")
+
+greet("kenneth")
+
+function joinNames(input_1, input_2) {
+    var join_string = input_1 + " " + input_2
+    var advanced_string = input_1 + input_1 + " | " + input_2 + input_2
+    return join_string
+
+}
+
+console.log(joinNames("zhiwei", "bala"))
